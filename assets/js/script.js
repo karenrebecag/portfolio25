f85015adf490fbb3db47eebef3a2a73466fca1dc
KarenOrtiz <karen.ortizg@yahoo.com>
2025-02-11 20:16:13 -0600

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -470,22 +470,4 @@ $(document).ready(function() {
     });
   });
 
-  var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    simulateTouch: true,
-    grabCursor: true,
-  });
+
